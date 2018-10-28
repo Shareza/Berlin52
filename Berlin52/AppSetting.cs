4adf3be035aab3aca24bc4e9ccb599cb2faf49b0
Shareza <michal.soja90@gmail.com>
2018-10-28 08:59:40 +0100

    Added Crossover, Mutation and Selection Types

--- Berlin52/AppSetting.cs
+++ Berlin52/AppSetting.cs
@@ -5,9 +5,15 @@
         public static string FilePath = @"..\..\Data.txt";
         public static int NumberOfGenes;
 
-        public static int PopulationSize = 5;
+        public static int PopulationSize = 40;
+
+        public static SelectionType selectionStrategy = SelectionType.PMXSelection;
         public static int SelectionRate = 3;
+
+        public static CrossoverType crossoverStrategy = CrossoverType.MultiPointCrossover;
         public static int CrossOverRate = 0;
+
+        public static MutationType mutationStrategy = MutationType.SwapMutation;
         public static int MutationRate = 0;
     }
 }
